4518a2689fcfc2a7dc38c592042d7c73ec98c486
christmas201743 <94320950+christmas201743@users.noreply.github.com>
2023-06-02 19:19:47 +0400

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,24 @@
+var button = document.getElementById('myButton');
+var modal = document.getElementById('myModal');
+var close = document.getElementById('myClose');
+var content = document.getElementById('myContent');
+
+document.getElementById('myTitle').style.color = "red";
+
+function opacity1() {
+    content.style.opacity = "1";
+}
+
+button.onclick = function() {
+    modal.style.display = "block";
+    setTimeout(opacity1, 100);
+}
+
+function none() {
+    modal.style.display = "none";
+}
+
+close.onclick = function() {
+    content.style.opacity = "0";
+    setTimeout(none, 1000);
+}
